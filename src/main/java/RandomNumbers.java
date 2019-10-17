1c06cb7081860efb3ad1d8d5de786e8a47f8e3f0
MalikIbadov <malikibadov97@gmail.com>
2019-10-17 09:00:56 +0400

    --cleared cache of gitignore

---
+++ src/main/java/RandomNumbers.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        for(int i = 0; i < 10; i++) {
+            System.out.print(rand.nextInt(10) + 10 + ", ");
+        }
+    }
+}
