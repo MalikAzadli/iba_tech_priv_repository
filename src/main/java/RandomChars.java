e2b1a8905683b422402f567af63af1fe3ea8fa8e
MalikIbadov <malikibadov97@gmail.com>
2019-10-22 09:00:01 +0400

    --Added changes

--- src/main/java/RandomChars.java
+++ src/main/java/RandomChars.java
@@ -3,13 +3,11 @@ import java.util.Random;
 
 public class RandomChars {
     public static void main(String[] args) {
-        int sizeSet = 5;
+        int sizeSet = 32;
         Random rand = new Random();
         ArrayList<Character> charList = new ArrayList<>();
         for(int i=0; i<sizeSet; i++){
-            int margin = 0;
-            if(rand.nextBoolean()) margin = 32;
-            int index = rand.nextInt((int)'Z' - (int)'A' + 1) + (int)'A' + margin;
+            int index = rand.nextInt((int)'Z' - (int)'A' + 1) + (int)'A' + rand.nextInt(2)*32;
             charList.add((char) index);
         }
         System.out.println(charList);
