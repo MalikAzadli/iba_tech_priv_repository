6900534b5cae152c50faf5f651891db631aab590
MalikIbadov <malikibadov97@gmail.com>
2019-10-22 08:49:49 +0400

    --Added RandomChars

---
+++ src/main/java/RandomChars.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomChars {
+    public static void main(String[] args) {
+        int sizeSet = 5;
+        Random rand = new Random();
+        ArrayList<Character> charList = new ArrayList<>();
+        for(int i=0; i<sizeSet; i++){
+            int margin = 0;
+            if(rand.nextBoolean()) margin = 32;
+            int index = rand.nextInt((int)'Z' - (int)'A') + (int)'A' + margin;
+            charList.add((char) index);
+        }
+        System.out.println(charList);
+    }
+}
