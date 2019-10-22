dc38f8839d7bb873b454baa40e6742ae39c7bd23
MalikIbadov <malikibadov97@gmail.com>
2019-10-22 09:06:31 +0400

    --Added method

--- src/main/java/RandomChars.java
+++ src/main/java/RandomChars.java
@@ -1,9 +1,18 @@
+
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class RandomChars {
     public static void main(String[] args) {
-        int sizeSet = 32;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Set size: ");
+        int size = scan.nextInt();
+        printRandomChars(size);
+    }
+
+    public static void printRandomChars(int size) {
+        int sizeSet = size;
         Random rand = new Random();
         ArrayList<Character> charList = new ArrayList<>();
         for(int i=0; i<sizeSet; i++){
