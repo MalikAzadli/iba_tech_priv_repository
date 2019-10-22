c2ecae3612cd7fae3caa790af28886f9f8f98957
MalikIbadov <malikibadov97@gmail.com>
2019-10-22 09:16:34 +0400

    --Added minor changes

--- src/main/java/RandomChars.java
+++ src/main/java/RandomChars.java
@@ -5,20 +5,21 @@ import java.util.Scanner;
 
 public class RandomChars {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Set size: ");
-        int size = scan.nextInt();
-        printRandomChars(size);
+        String s1 = genRandom(10);
+        String s2 = genRandom(5);
+        System.out.println(s1);
+        System.out.println(s2);
     }
 
-    public static void printRandomChars(int size) {
+    public static String genRandom(int size) {
         int sizeSet = size;
         Random rand = new Random();
-        ArrayList<Character> charList = new ArrayList<>();
+        String stream = "";
         for(int i=0; i<sizeSet; i++){
             int index = rand.nextInt((int)'Z' - (int)'A' + 1) + (int)'A' + rand.nextInt(2)*32;
-            charList.add((char) index);
+            stream += ((char) index);
         }
-        System.out.println(charList);
+
+        return stream;
     }
 }
