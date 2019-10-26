3867d8d2a1f1043201347d656b74ed1f48dcd002
MalikIbadov <malikibadov97@gmail.com>
2019-10-26 15:00:11 +0400

    --added RandomIterator - not completed

--- src/main/java/warmup/oct26/iterator1/HiddenApp.java
+++ src/main/java/warmup/oct26/iterator1/HiddenApp.java
@@ -5,23 +5,22 @@ import java.util.Iterator;
 public class HiddenApp {
     public static void main(String[] args) {
         HiddenData months = new HiddenData();
+//
+//        for (String month : months) {
+//            System.out.printf("** %s **\n", month);
+//        }
+//
+//        Iterator<String> it = months.reverseIterator();
+//        while (it.hasNext()) {
+//            System.out.printf(">> %s <<\n", it.next());
+//        }
+//
+//        months.forEach(m -> System.out.printf("<< %s >>\n", m));
+//
+        Iterator<String> randomIterator = months.randomIterator();
 
-        for (String month: months) {
-            System.out.printf("** %s **\n", month);
+        while (randomIterator.hasNext()) {
+            System.out.println(randomIterator.next());
         }
-
-        Iterator<String> it = months.iterator();
-        while (it.hasNext()) {
-            System.out.printf(">> %s <<\n", it.next());
-        }
-
-        months.forEach(m -> System.out.printf("<< %s >>\n", m));
-
-        Iterator<String> reverseIt = months.reverseIterator();
-        while (reverseIt.hasNext()){
-            System.out.println(reverseIt.next());
-        }
-
-        months.reverseIterator();
     }
 }
--- src/main/java/warmup/oct26/iterator1/HiddenData.java
+++ src/main/java/warmup/oct26/iterator1/HiddenData.java
@@ -3,10 +3,11 @@ package warmup.oct26.iterator1;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 public class HiddenData implements Iterable<String> {
     private final List<String> months = Arrays.asList(
-            "Jan","Feb","Mar","Apr","May");
+            "Jan", "Feb", "Mar", "Apr", "May");
 
     @Override
     public Iterator<String> iterator() {
@@ -31,15 +32,33 @@ public class HiddenData implements Iterable<String> {
 
     public Iterator<String> reverseIterator() {
         Iterator<String> iterator = new Iterator<String>() {
-            int index = months.size()-1;
+            int index = months.size() - 1;
+
             @Override
             public boolean hasNext() {
-                return index < 0;
+                return index > -1;
             }
 
             @Override
             public String next() {
                 return months.get(index--);
+            }
+        };
+        return iterator;
+    }
+
+    public Iterator<String> randomIterator() {
+        Iterator<String> iterator = new Iterator<String>() {
+            Random rand = new Random();
+
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public String next() {
+                return null;
             }
         };
         return iterator;
