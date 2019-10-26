3614d05f167472dce6f57c584f744559f52fc595
MalikIbadov <malikibadov97@gmail.com>
2019-10-26 11:41:52 +0400

    --removed getIndices() --simplified getPairs()

--- src/main/java/warmup/oct26/LetterAnalyzer.java
+++ src/main/java/warmup/oct26/LetterAnalyzer.java
@@ -1,43 +1,31 @@
 package warmup.oct26;
 
-import mix.RandomChars;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class LetterAnalyzer {
 
-    private String text;
+    private final String text;
     private ArrayList<Integer> currentArray;
 
-    public LetterAnalyzer(String text){
+    public LetterAnalyzer(String text) {
         this.text = text;
     }
 
-    public void getIndices(Character ch){
-        for(int i = 0; i < this.text.length(); i++){
-            if(ch.equals(text.charAt(i))) currentArray.add(i+1);
-        }
-    }
-
-    public Map<Character, ArrayList<Integer>> getPairs(){
+    public Map<Character, ArrayList<Integer>> getPairs() {
         Map<Character, ArrayList<Integer>> map = new HashMap<>();
-        for(int i = 0; i < this.text.length(); i++){
+        for (int i = 0; i < this.text.length(); i++) {
             Character character = this.text.charAt(i);
-            this.currentArray = new ArrayList<>();
-            if(map.containsKey(character)) continue;
-            getIndices(character);
-            map.put(character, currentArray);
+            if (map.containsKey(character)) map.get(character).add(i + 1);
+            else map.put(character, new ArrayList<Integer>(Arrays.asList(i + 1)));
         }
         return map;
     }
 
-    public void printMap(Map<Character, ArrayList<Integer>> map){
+    public void printMap(Map<Character, ArrayList<Integer>> map) {
         Set<Character> characters = map.keySet();
-        for(char c: characters){
+        for (char c : characters) {
             ArrayList<Integer> indices = map.get(c);
-            System.out.println(c + ": " + indices.size()+"; " + indices.toString());
+            System.out.println(c + ": " + indices.size() + "; " + indices.toString());
         }
     }
 
