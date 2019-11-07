5842182d177a5b5fbab23b0bff272ea3327449ce
Aleksey <alekseyh215@gmail.com>
2019-11-07 12:25:03 +0400

    --classNov07 added

---
+++ src/test/java/classNov07/warmup/SolutionTest.java
@@ -0,0 +1,31 @@
+package classNov07.warmup;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void solution1() {
+        Solution solution = new Solution();
+        assertEquals(4, solution.solution("RLRRLLRLRRLL"));
+    }
+
+    @Test
+    public void solution2() {
+        Solution solution = new Solution();
+        assertEquals(1, solution.solution("LLRLRLRLRLRLRR"));
+    }
+
+
+    @Test
+    public void solution3() {
+        Solution solution = new Solution();
+        assertEquals(4, solution.solution("RLLLRRRLLR"));
+    }
+
+
+
+
+}
