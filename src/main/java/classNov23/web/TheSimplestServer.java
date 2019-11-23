827cd574f17df095209239cc61e89dbc366ae3da
MalikIbadov <malikibadov97@gmail.com>
2019-11-23 12:10:20 +0400

    --web started

---
+++ src/main/java/classNov23/web/TheSimplestServer.java
@@ -0,0 +1,16 @@
+package classNov23.web;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+public class TheSimplestServer {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(9001);
+        ServletContextHandler handler = new ServletContextHandler();
+
+        handler.addServlet(HelloServlet.class, "/*");
+        server.setHandler(handler);
+        server.start();
+        server.join();
+    }
+}
